c1e4f3633e9633609c0175c6076b0367aa670e46
Mateus Santos Costa <mateussant0sc0osta@outlook.com>
2025-10-15 11:29:48 -0300

    mas um dia de aula

---
+++ Atividade 10/script.js
@@ -0,0 +1,30 @@
+function adicionaritem() {
+  
+    var input = document.getElementById("item");
+    var texto = input.value.trim();
+
+    if (texto !== "") {
+        
+        var lista = document.getElementById("lista");
+        var novoitem = document.createElement("li");
+        novoitem.textContent = texto;
+        lista.appendChild(novoitem);
+        
+        
+        input.value = ""; 
+    }
+}
+
+function removeritem() {
+    
+    var lista = document.getElementById("lista");
+    var ultimoitem = lista.lastElementChild;
+    if (ultimoitem) {
+        ultimoitem.remove();
+    }
+}
+
+
+var botao = document.getElementById("botao");
+botao.onclick = adicionaritem;,
+botao.ondblclick = removeritem;
