52327a82d605c7604bb1d13ea7b616a3e71281ec
Victor-Denv <vida12345678904@gmail.com>
2025-11-06 08:49:01 -0300

    Adiciona arquivos de projeto e configurações iniciais para a Atividade Em C Sharp

---
+++ Atividade Em C Sharp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+
+int idade;
+double altura;
+char genero;
+string nome;
+
+
+Console.Write("Informe o Seu Nome : ");
+nome = Console.ReadLine();
+Console.Write("Nome Informado : " + nome);
